900573a057750cffe2b9e80c7fad54657c756421
Brian Seaver <bseaver@d155.org>
2021-07-08 09:12:47 -0500

    Adding athletes to an event works completely.  Now working on deleting from event. Had to comment out SwipeToDeleteCallback

--- app/src/main/java/com/example/androidquicktrackmeet/SwipeToDeleteCallback.java
+++ app/src/main/java/com/example/androidquicktrackmeet/SwipeToDeleteCallback.java
@@ -3,18 +3,18 @@ package com.example.androidquicktrackmeet;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
-    private EditEventListAdapter mAdapter;
-
-    public SwipeToDeleteCallback(EditEventListAdapter adapter) {
-        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
-        mAdapter = adapter;
-    }
-
-    @Override
-    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-        int position = viewHolder.getAdapterPosition();
-        mAdapter.deleteItem(position);
-    }
-
-}
+//public class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
+//    private EditEventListAdapter mAdapter;
+//
+//    public SwipeToDeleteCallback(EditEventListAdapter adapter) {
+//        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+//        mAdapter = adapter;
+//    }
+//
+//    @Override
+//    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+//        int position = viewHolder.getAdapterPosition();
+//        mAdapter.deleteItem(position);
+//    }
+//
+//}
